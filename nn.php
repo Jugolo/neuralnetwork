7f184515b4095df9ea9aa9a7e84ebbcaea0afd96
Ronnie <rix17172@gmail.com>
2017-03-08 09:03:14 +0100

    Update nn.php

--- nn.php
+++ nn.php
@@ -1,2 +1,28 @@
 <?php
  
+function createArray(int $neuron, int $layer = 1) : array{
+ $buffer = [];
+ for($i=0;$i<$layer;$i++){
+  $buf = [];
+  for($b=0;$b<$neuron;$b++){
+   $buf[] = 0;
+  }
+  $buffer[] = $buf;
+ }
+ return $buffer;
+}
+
+function layerSize(array $array) : int{
+ return count($array);
+}
+
+function neuronSize(array $array) : int{
+ if(layerSize($array) == 0){
+  return 0;
+ }
+ return count($array[0]);
+}
+
+function completeSize(array $array) : int{
+ return layerSize($array) * neuronSize($array);
+}
